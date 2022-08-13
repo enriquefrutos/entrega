38d8b6f502bce061fe9f44350c388a531512d059
henry <enriqueafrutos@gmail.com>
2022-08-13 17:55:04 -0300

    agrego css

--- app2.js
+++ app2.js
@@ -1,3 +1,13 @@
+let infototal = localStorage.getItem('total');
+let ainfo = JSON.parse(infototal);
+function totalinfo(){
+  if (ainfo) {let li = document.createElement("li");
+  li.innerHTML = `Ya en el carrito $${ainfo}`;
+  total.append(li);
+  }
+}
+totalinfo(ainfo);
+
 let botond = document.getElementById('botond')
 botond.addEventListener('click',verpelis);
 function verpelis(){
@@ -15,4 +25,17 @@ function verpelis(){
 if (todPelis.hasChildNodes()){ }
 else {   
 pedirDatos();}}
-    
+
+let suma = 0;
+let nuevototal =0;
+let comprar = document.getElementById('compras')
+let totalcompra = document.createElement ("li");
+comprar.addEventListener('click', comprarentrada);
+function comprarentrada (){
+  suma += 1500
+  nuevototal = ainfo + suma
+totalcompra.innerHTML = `Total compra $${nuevototal}`;
+total.append(totalcompra);
+localStorage.setItem("total", suma)
+}
+
